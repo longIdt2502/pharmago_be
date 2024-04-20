f90c43741c698c8fbd8e7a0b99e21e74e9edd8e1
longIdt2502 <long.lbh@idtinc.co>
2024-04-20 17:11:18 +0700

    notification: api list

--- gapi/mapper/service_mapper.go
+++ gapi/mapper/service_mapper.go
@@ -6,15 +6,22 @@ import (
 )
 
 func ServiceMapper(item db.Service) *pb.Service {
+
+	var time *int32
+	if item.ReminderTime.Valid {
+		time = &(item.ReminderTime.Int32)
+	}
+
 	return &pb.Service{
-		Id:          item.ID,
-		Code:        item.Code,
-		Title:       item.Title,
-		Entity:      &item.Entity.String,
-		Frequency:   &item.Frequency.String,
-		Unit:        item.Unit,
-		Price:       float32(item.Price),
-		Description: &item.Description.String,
-		Company:     item.Company,
+		Id:           item.ID,
+		Code:         item.Code,
+		Title:        item.Title,
+		Entity:       &item.Entity.String,
+		Frequency:    &item.Frequency.String,
+		Unit:         item.Unit,
+		Price:        float32(item.Price),
+		Description:  &item.Description.String,
+		Company:      item.Company,
+		ReminderTime: time,
 	}
 }
